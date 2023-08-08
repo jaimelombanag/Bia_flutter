6cb2f10a6b3949f5414b99ae2b1ae8161fc06bfa
Jaime Andres  Lombana Gonzalez <jaimelombana@MacBook-Pro-de-Jaime-2.local>
2023-08-08 11:26:33 -0500

    icon launcher ios and android

--- bia_energy_prueba/lib/src/themes/app_themes_colors.dart
+++ bia_energy_prueba/lib/src/themes/app_themes_colors.dart
@@ -1,23 +1,18 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ThemeColor {
   static const Color white = Color(0xffffffff);
   static const Color secondaryGrey = Color(0xffF1F3F4);
   static const Color secondaryMediumGrey = Color(0xff0000004D);
-  static const Color primaryBlack = Color(0xff141915);
-  static const Color secondaryDarkGrey = Color(0xff606260);
+  static const Color primaryBlack = Color(0xff0A1023);
+  static const Color secondaryDarkBlue = Color(0xff221A7C);
   static const Color primaryDarkGrey = Color(0xff414141);
-  static const Color primaryBlue = Color(0xFF2E2739);
-  static const Color primaryGreen = Color(0xff5EBE4E);
+  static const Color primaryGreen = Color(0xff75FBFD);
   static const Color primaryGrey = Colors.grey;
   static const Color secondaryBlack = Color(0xff2B2F2C);
   static const Color secondaryRed = Color(0xffE2173A);
   static const Color primaryShadowGrey = Color(0xffBABABA);
   static const Color primaryYellow = Color(0xffE2B317);
-  static const Color gradient1 = Color(0xFF2E2739);
-  static const Color gradient2 = Color(0xFF49BEE8);
-  static Color tabsBackground = Color(0xff239DD1);
 
   ThemeData get themeData {
     ColorScheme colorSchemeMovieApp = const ColorScheme(
